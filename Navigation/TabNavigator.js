174a5a45f5c6c8d0e9826a09314f41485a5fea8f
AngelSharma-hash <81636976+AngelSharma-hash@users.noreply.github.com>
2022-01-03 22:50:18 +0530

    Add files via upload

---
+++ Navigation/TabNavigator.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet, 
+} from "react-native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../Screens/Feedback";
+import CreatePost from "../Screens/CreatePost";
+
+
+const Tab = createBottomTabNavigator();
+const BottomTabNavigator= () =>{
+    return(
+        <Tab.Navigator
+          screenOptions={({route})=>({
+            tabBarIcon:({focused, color, size})=>{
+                let iconName;
+                if(route.name === 'Feed'){
+                    iconName = focused ?'book': 'book-outline';
+                }
+                else if(route.name === 'CreatePost'){
+                    iconName = focused ? 'create': 'create-outline';
+                }
+                return( <Ionicons name={iconName} size={size} color={color} />)
+            },        
+            
+          })}
+          tabBarOptions={{
+            activeTintColor:'tomato',
+            inactiveTintColor:'gray',
+        }}  >
+            <Tab.Screen name="Feed" component={Feed}/>
+            <Tab.Screen name="CreatePost" component={CreatePost}/>
+        </Tab.Navigator>
+    );
+}
+
+export default BottomTabNavigator
+
+
+
+
+const styles = StyleSheet.create({
+   
+});
